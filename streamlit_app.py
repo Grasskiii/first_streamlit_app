3f0436214e1700fca871770d86d6703f5471b03c
Grasskiii <126506057+Grasskiii@users.noreply.github.com>
2023-02-27 13:52:19 -0700

    Update streamlit_app.py
    Only show selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,9 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Allows user to puck their fruit
-streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+# Allows user to pick their fruit
+fruit_selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Displays the table of fruit
 streamlit.dataframe(my_fruit_list)
