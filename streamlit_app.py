17b5ac03597b3ecf807a3516c99d2a037c9bb803
Grasskiii <126506057+Grasskiii@users.noreply.github.com>
2023-02-27 13:53:49 -0700

    Update streamlit_app.py
    Fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ fruits_selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Displays the table of fruit
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
